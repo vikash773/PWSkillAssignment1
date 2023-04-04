9839f19d6b6c0fe1c96ca8f5c85fe663e5f81b44
Vikash Kumar Bharti <129424883+vikash773@users.noreply.github.com>
2023-04-04 10:43:22 +0530

    Add files via upload

---
+++ Ans1.java
@@ -0,0 +1,8 @@
+//Q1 - Take 2 integer values in two variables x and y and print their product.
+class Ans1{
+    public static void main(String[] args) {
+        int x=2;
+        int y=4;
+        System.out.println(x*y);
+    }
+}
